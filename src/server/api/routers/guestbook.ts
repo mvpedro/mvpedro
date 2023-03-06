67936e558083ca277077b8f02b66a99911e31022
mvpedro <mvpedro.cc@gmail.com>
2023-03-06 07:24:55 -0300

    Adding type error fixes

--- src/server/api/routers/guestbook.ts
+++ src/server/api/routers/guestbook.ts
@@ -1,30 +1,24 @@
 import { z } from "zod";
-import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const guestbookRouter = createTRPCRouter({
-    getAll: publicProcedure.query(async ({ ctx }) => {
-        try {
-          return await ctx.prisma.guestbook.findMany({
-            select: {
-              name: true,
-              message: true,
-            },
-            orderBy: {
-              createdAt: "desc",
-            },
-          });
-        } catch (error) {
-          console.log("error", error);
-        }
-      }),
-
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    try {
+      return await ctx.prisma.guestbook.findMany({
+        select: {
+          name: true,
+          message: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }),
   postMessage: protectedProcedure
-    .input(
-      z.object({
-        name: z.string(),
-        message: z.string(),
-      })
-    )
+    .input(z.object({ name: z.string(), message: z.string() }))
     .mutation(async ({ ctx, input }) => {
       try {
         await ctx.prisma.guestbook.create({
